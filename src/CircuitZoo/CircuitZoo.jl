5a28c9b1dd2a36a20697d322aa8bf7c1f8ef08fe
Adrian Ariton <adrianariton@192-168-0-154.rdsnet.ro>
2023-08-04 16:30:20 +0300

    scaffolding for splitting stringend for amazing brther Andu

--- src/CircuitZoo/CircuitZoo.jl
+++ src/CircuitZoo/CircuitZoo.jl
@@ -79,7 +79,33 @@ struct Purify3to1 <: AbstractCircuit
     end
 end
 
-struct PurifyStringent <: AbstractCircuit
+function (circuit::Purify3to1)(purifiedL,purifiedR,sacrificedL::Array,sacrificedR::Array)
+    gate1, gate2, basis1, basis2, parity1, parity2 = if circuit.fixtwice==:X
+        YCZ, ZCX, σʸ, σᶻ, 1, 0
+    elseif circuit.fixtwice==:Y
+        ZCX, XCZ, σᶻ, σˣ, 0, 0
+    elseif circuit.fixtwice==:Z
+        XCZ, YCZ, σˣ, σʸ, 0, 1
+    end
+
+    apply!((purifiedL, sacrificedL[1]),gate1)
+    apply!((purifiedR, sacrificedR[1]),gate1)
+
+    apply!((sacrificedR[1],sacrificedR[2]),gate2)
+    apply!((sacrificedL[1],sacrificedL[2]),gate2)
+
+    measa1 = project_traceout!(sacrificedL[1], basis1)
+    measb1 = project_traceout!(sacrificedR[1], basis1)
+    measa2 = project_traceout!(sacrificedL[2], basis2)
+    measb2 = project_traceout!(sacrificedR[2], basis2)
+    success1 = (measa1 ⊻ measb1 == parity1)
+    success2 = (measa2 ⊻ measb2 == parity2)
+
+    if !(success1 && success2)
+        traceout!(purifiedL)
+        traceout!(purifiedR)
+    end
+    (success1 && success2)
 end
 
 function coin(basis, pair::Array, parity=0)
@@ -89,7 +115,71 @@ function coin(basis, pair::Array, parity=0)
     success
 end
 
+struct StringentHead <: AbstractCircuit
+    type::Symbol
+    function StringentHead(type)
+        if type ∉ (:X, :Z)
+            throw(ArgumentError(lazy"""
+            `type` has to be one of `:X`, or `:Z`.
+            You have instead chosen `$(repr(fixtwice))` which is not a valid option.
+            Investigate where you are creating a purification circuit of type `StringentHead`
+            and ensure you are passing a valid argument.
+            """))
+        else
+            new(type)
+        end
+    end
+end
+
+function (circuit::StringentHead)(purifiedL, purifiedR, sacrificedL, sacrificedR)
+    gate, success = if circuit.type == :Z
+        ZCZ, true
+    else
+        XCZ, true
+    end
+    apply!((purifiedL, sacrificedL[1]), gate)
+    apply!((purifiedR, sacrificedR[1]), gate)
+    apply!((sacrificedR[1], sacrificedR[2]), ZCZ)
+    apply!((sacrificedL[1], sacrificedL[2]), ZCZ)
+
+    success = success & !coin(σˣ, [sacrificedL[1], sacrificedR[1]])
+    success = success & !coin(σˣ, [sacrificedL[2], sacrificedR[2]])
+
+    success
+end
+
+struct StringentBody <: AbstractCircuit
+    type::Symbol
+    function StringentHead(type)
+        if type ∉ (:X, :Z)
+            throw(ArgumentError(lazy"""
+            `type` has to be one of `:X`, or `:Z`.
+            You have instead chosen `$(repr(fixtwice))` which is not a valid option.
+            Investigate where you are creating a purification circuit of type `StringentBody`
+            and ensure you are passing a valid argument.
+            """))
+        else
+            new(type)
+        end
+    end
+end
+
+function (circuit::StringentBody)(purifiedL, purifiedR, sacrificedL, sacrificedR)
+    gate = if circuit.type == :Z
+        ZCZ
+    else
+        ZCX
+    end
+
+    # TODO;;
+end
+
+struct PurifyStringent <: AbstractCircuit
+end
+
 function (circuit::PurifyStringent)(purifiedL,purifiedR,sacrificedL,sacrificedR)
+    # 2 x {:X,:Z} StringentBody ; 2 x {:X,:Z} StringentHead
+    # TODO: wait for nodes to fill
     gate1, gate2 = ZCZ, XCZ
     basis = X
     free_index = 1
@@ -246,35 +336,6 @@ function (circuit::PurifyStringent)(purifiedL,purifiedR,sacrificedL,sacrificedR)
 
     success
 
-end
-
-function (circuit::Purify3to1)(purifiedL,purifiedR,sacrificedL::Array,sacrificedR::Array)
-    gate1, gate2, basis1, basis2, parity1, parity2 = if circuit.fixtwice==:X
-        YCZ, ZCX, σʸ, σᶻ, 1, 0
-    elseif circuit.fixtwice==:Y
-        ZCX, XCZ, σᶻ, σˣ, 0, 0
-    elseif circuit.fixtwice==:Z
-        XCZ, YCZ, σˣ, σʸ, 0, 1
-    end
-
-    apply!((purifiedL, sacrificedL[1]),gate1)
-    apply!((purifiedR, sacrificedR[1]),gate1)
-
-    apply!((sacrificedR[1],sacrificedR[2]),gate2)
-    apply!((sacrificedL[1],sacrificedL[2]),gate2)
-
-    measa1 = project_traceout!(sacrificedL[1], basis1)
-    measb1 = project_traceout!(sacrificedR[1], basis1)
-    measa2 = project_traceout!(sacrificedL[2], basis2)
-    measb2 = project_traceout!(sacrificedR[2], basis2)
-    success1 = (measa1 ⊻ measb1 == parity1)
-    success2 = (measa2 ⊻ measb2 == parity2)
-
-    if !(success1 && success2)
-        traceout!(purifiedL)
-        traceout!(purifiedR)
-    end
-    (success1 && success2)
 end
 
 end # module
