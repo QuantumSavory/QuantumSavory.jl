a0ef05e1e090a19b48c406cc56f0c15531b3cf4d
Alexandru Ariton <anduariton1234@gmail.com>
2023-08-10 17:13:06 +0300

    Added Testing

--- src/CircuitZoo/CircuitZoo.jl
+++ src/CircuitZoo/CircuitZoo.jl
@@ -132,4 +132,189 @@ function (circuit::Purify2to1)(purifiedL,purifiedR,sacrificedL,sacrificedR)
     success
 end
 
+struct Purify3to1 <: AbstractCircuit
+    fixtwice::Symbol
+    function Purify3to1(fixtwice)
+        if fixtwice ∉ (:X, :Y, :Z)
+            throw(ArgumentError(lazy"""
+            `Purify3to1` can represent one of three purification circuits (see its docstring),
+            parameterized by the argument `fixtwice` which has to be one of `:X`, `:Y`, or `:Z`.
+            You have instead chosen `$(repr(fixtwice))` which is not a valid option.
+            Investigate where you are creating a purification circuit of type `Purify3to1`
+            and ensure you are passing a valid argument.
+
+            The `fixtwice` parameter represents the error that the putifying circuit will 
+            'fix twice', meaning for example, for a fixtwice Y circuit (original double selection),
+            Purify3to1 will represent the succesive applying of a leaveout Z and then a leaveout X
+            Purify2to1 circuit, with coincidence measurments pushed to the end.
+            """))
+        else
+            new(fixtwice)
+        end
+    end
+end
+
+function (circuit::Purify3to1)(purifiedL,purifiedR,sacrificedL::Array,sacrificedR::Array)
+    gate1, gate2, basis1, basis2 = if circuit.fixtwice==:X
+        ZCY, XCZ, σʸ, σᶻ
+    elseif circuit.fixtwice==:Y
+        XCZ, CNOT, σᶻ, σˣ
+    elseif circuit.fixtwice==:Z
+        CNOT, ZCY, σˣ, σʸ
+    end
+
+    apply!((sacrificedL[1], purifiedL),gate1)
+    apply!((sacrificedR[1], purifiedR),gate1)
+
+    apply!((sacrificedR[2],sacrificedR[1]),gate2)
+    apply!((sacrificedL[2],sacrificedL[1]),gate2)
+
+    measa1 = project_traceout!(sacrificedL[1], basis1)
+    measb1 = project_traceout!(sacrificedR[1], basis1)
+    measa2 = project_traceout!(sacrificedL[2], basis2)
+    measb2 = project_traceout!(sacrificedR[2], basis2)
+    
+    success1 = measa1 == measb1
+    success2 = measa2 == measb2
+
+    if !(success1 && success2)
+        traceout!(purifiedL)
+        traceout!(purifiedR)
+    end
+    (success1 && success2)
+end
+
+function coin(basis, pair::Array, parity=0)
+    measa = project_traceout!(pair[1], basis)
+    measb = project_traceout!(pair[2], basis)
+    success = (measa ⊻ measb == parity)
+    success
+end
+
+struct StringentHead <: AbstractCircuit
+    type::Symbol
+    function StringentHead(type)
+        if type ∉ (:X, :Z)
+            throw(ArgumentError(lazy"""
+            `type` has to be one of `:X`, or `:Z`.
+            You have instead chosen `$(repr(fixtwice))` which is not a valid option.
+            Investigate where you are creating a purification circuit of type `StringentHead`
+            and ensure you are passing a valid argument.
+            """))
+        else
+            new(type)
+        end
+    end
+end
+
+function (circuit::StringentHead)(purifiedL, purifiedR, sacrificedL, sacrificedR)
+    gate, success = if circuit.type == :Z
+        ZCZ, true
+    else
+        XCZ, true
+    end
+    apply!((purifiedL, sacrificedL[1]), gate)
+    apply!((purifiedR, sacrificedR[1]), gate)
+    apply!((sacrificedR[1], sacrificedR[2]), ZCZ)
+    apply!((sacrificedL[1], sacrificedL[2]), ZCZ)
+
+    success = success & coin(σˣ, [sacrificedL[1], sacrificedR[1]])
+    success = success & coin(σˣ, [sacrificedL[2], sacrificedR[2]])
+
+    success
+end
+
+struct StringentBody <: AbstractCircuit
+    type::Symbol
+    expedient::Bool
+    function StringentBody(type, expedient=false)
+        if type ∉ (:X, :Z)
+            throw(ArgumentError(lazy"""
+            `type` has to be one of `:X`, or `:Z`.
+            You have instead chosen `$(repr(fixtwice))` which is not a valid option.
+            Investigate where you are creating a purification circuit of type `StringentBody`
+            and ensure you are passing a valid argument.
+            """))
+        else
+            new(type, expedient)
+        end
+    end
+end
+
+function (circuit::StringentBody)(purifiedL, purifiedR, sacrificedL, sacrificedR)
+    gate, success = if circuit.type == :Z
+        ZCZ, true
+    else
+        XCZ, true
+    end
+    ## Indices for emulating pair creation
+    i1 = 1
+    i2 = 1
+    sacrificedL1 = sacrificedL[1:1]
+    sacrificedR1 = sacrificedR[1:1]
+
+    sacrificedL2 = circuit.expedient ? sacrificedL[2:3] : sacrificedL[2:4]
+    sacrificedR2 = circuit.expedient ? sacrificedR[2:3] : sacrificedR[2:4]
+
+    apply!((sacrificedL1[i1], sacrificedL2[i2]), XCZ)
+    apply!((sacrificedR1[i1], sacrificedR2[i2]), XCZ)
+    success = success & coin(σˣ, [sacrificedL2[i2], sacrificedR2[i2]])
+    i2 = i2 + 1
+
+    apply!((sacrificedL1[i1], sacrificedL2[i2]), ZCZ)
+    apply!((sacrificedR1[i1], sacrificedR2[i2]), ZCZ)
+    success = success & coin(σˣ, [sacrificedL2[i2], sacrificedR2[i2]])
+
+    apply!((purifiedL, sacrificedL1[i1]), gate)
+    apply!((purifiedR, sacrificedR1[i1]), gate)
+
+    if !circuit.expedient 
+        i2 = i2 + 1
+        apply!((sacrificedL1[i1], sacrificedL2[i2]), ZCZ)
+        apply!((sacrificedR1[i1], sacrificedR2[i2]), ZCZ)
+
+        success = success & coin(σˣ, [sacrificedL2[i2], sacrificedR2[i2]])
+    end
+
+    success = success & coin(σˣ, [sacrificedL1[i1], sacrificedR1[i1]])
+
+    success
+
+end
+
+struct PurifyStringent <: AbstractCircuit
+end
+
+function (circuit::PurifyStringent)(purifiedL,purifiedR,sacrificedL,sacrificedR)
+    success = true
+    stringentHead_Z = StringentHead(:Z)
+    stringentHead_X = StringentHead(:X)
+    stringentBody_Z = StringentBody(:Z)
+    stringentBody_X = StringentBody(:X)
+
+    success = success & stringentHead_Z(purifiedL, purifiedR, sacrificedL[1:2], sacrificedR[1:2])
+    success = success & stringentHead_X(purifiedL, purifiedR, sacrificedL[3:4], sacrificedR[3:4])
+    success = success & stringentBody_Z(purifiedL, purifiedR, sacrificedL[5:8], sacrificedR[5:8])
+    success = success & stringentBody_X(purifiedL, purifiedR, sacrificedL[9:12], sacrificedR[9:12])
+    
+    success
+end
+
+struct PurifyExpedient <: AbstractCircuit
+end
+
+function (circuit::PurifyExpedient)(purifiedL,purifiedR,sacrificedL,sacrificedR)
+    success = true
+    stringentHead_Z = StringentHead(:Z)
+    stringentHead_X = StringentHead(:X)
+    stringentBody_Z = StringentBody(:Z, true)
+
+    success = success & stringentHead_Z(purifiedL, purifiedR, sacrificedL[1:2], sacrificedR[1:2])
+    success = success & stringentHead_X(purifiedL, purifiedR, sacrificedL[3:4], sacrificedR[3:4])
+    success = success & stringentBody_Z(purifiedL, purifiedR, sacrificedL[5:7], sacrificedR[5:7])
+    success = success & stringentBody_Z(purifiedL, purifiedR, sacrificedL[8:10], sacrificedR[8:10])
+    
+    success
+end
+
 end # module
--- test/test_circuitzoo_purification.jl
+++ test/test_circuitzoo_purification.jl
@@ -1,13 +1,75 @@
 using QuantumSavory
 using QuantumSavory.CircuitZoo
 using Test
+using QuantumSavory.CircuitZoo: EntanglementSwap, Purify2to1, Purify3to1, PurifyStringent, PurifyExpedient
+
+
+const T2=0
+const perfect_pair = (Z1⊗Z1 + Z2⊗Z2) / sqrt(2)
+const perfect_pair_dm = SProjector(perfect_pair)
+const mixed_dm = MixedState(perfect_pair_dm)
+noisy_pair_func(F) = F*perfect_pair_dm + (1-F)*mixed_dm
+
+function entangled_registers(size, purity, T2=0)
+    regA = Register([Qubit() for  _ in 1:size],
+                    [QuantumOpticsRepr() for  _ in 1:size], 
+                    [T2Dephasing(T2) for  _ in 1:size])
+    regB = Register([Qubit() for  _ in 1:size],
+                    [QuantumOpticsRepr() for  _ in 1:size], 
+                    [T2Dephasing(T2) for  _ in 1:size])
+
+    for i in 1:size
+        initialize!((regA[i],regB[i]), noisy_pair_func(purity))
+    end
+    regA, regB
+end
+
+# @testset "Keep Pure I Entanglement with :Y" begin
+#     @testset "Purify2to1" begin
+#         for _ in 1:10
+#             regA, regB = entangled_registers(2, 1)
+#             purificationcircuit = Purify2to1(:Y) # original single selection
+#             success = purificationcircuit(regA[1], regB[1], regA[2], regB[2])
+#             @test success == true
+#         end
+#     end
+
+#     @testset "Purify3to1" begin
+#         for _ in 1:10
+#             regA, regB = entangled_registers(3, 1)
+#             purificationcircuit = Purify3to1(:Y) # original double selection
+#             success = purificationcircuit(regA[1], regB[1], [regA[2], regA[3]], [regB[2], regB[3]])
+#             @test success == true
+#         end
+#     end
+
+#     @testset "PurifyStringent" begin
+#         for _ in 1:10
+#             regA, regB = entangled_registers(13, 1)
+#             purificationcircuit = PurifyStringent() # original single selection
+#             success = purificationcircuit(regA[1], regB[1], regA[2:13], regB[2:13])
+#             @test success == true
+#         end
+#     end
+
+#     @testset "PurifyExpedient" begin
+#         for _ in 1:10
+#             regA, regB = entangled_registers(11, 1)
+#             purificationcircuit = PurifyExpedient() # original single selection
+#             success = purificationcircuit(regA[1], regB[1], regA[2:11], regB[2:11])
+#             @test success == true
+#         end
+#     end
+# end
+
+
 
 const bell = StabilizerState("XX ZZ")
 # or equivalently `const bell = (Z₁⊗Z₁+Z₂⊗Z₂)/√2`,
 # however converting to stabilizer state for Clifford simulations
 # is not implemented (and can not be done efficiently).
 
-@test_throws ArgumentError Purify2to1(:lalala)
+# @test_throws ArgumentError Purify2to1(:lalala)
 
 @testset "2to1" begin
     for rep in [QuantumOpticsRepr, CliffordRepr]
@@ -32,6 +94,26 @@ const bell = StabilizerState("XX ZZ")
                     @test Purify2to1(leaveout)(r[1:4]...)==false
                 end
             end
+        end
+    end
+end
+
+@testset "3to1" begin
+    for rep in [QuantumOpticsRepr, CliffordRepr]
+        for fixtwice in [:X, :Y, :Z]
+            # test that pure state gets mapped to pure state
+            r = Register(6, rep())
+            initialize!(r[1:6], bell⊗bell⊗bell)
+            @test Purify3to1(fixtwice)(r[1], r[2], [r[3], r[5]], [r[4], r[6]])==true
+            @test observable(r[1:2], projector(bell))≈1.0
+
+            for error in [:X, :Y, :Z], target in 3:6
+                r = Register(6, rep())
+                initialize!(r[1:6], bell⊗bell⊗bell)
+                apply!(r[target], Dict(:X=>X, :Y=>Y, :Z=>Z)[error])
+                @test Purify3to1(fixtwice)(r[1], r[2], [r[3], r[5]], [r[4], r[6]])==false
+            end
+            
         end
     end
 end
