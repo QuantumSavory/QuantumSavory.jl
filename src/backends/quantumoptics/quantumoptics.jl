82ecccdffaa1c52973cde877fb0bb59d462ce4f4
Abhishek Bhatt <abhishekbhatt730@gmail.com>
2023-08-26 16:46:42 -0400

    more descriptive error message

--- src/backends/quantumoptics/quantumoptics.jl
+++ src/backends/quantumoptics/quantumoptics.jl
@@ -41,7 +41,9 @@ function project_traceout!(state::Union{Ket,Operator},stateindex,psis::Union{Tup
     if nsubsystems(state) == 1 # TODO is there a way to do this in a single function, instead of _overlap vs _project_and_drop
         _overlaps = [_overlap(psi,state) for psi in psis]
         branch_probs = cumsum(_overlaps)
-        @assert branch_probs[end] ≈ 1.0
+        if !(branch_probs[end] ≈ 1.0)
+            throw("State not normalized. Could be due to passing wrong state to `initialize!`")
+        end
         j = findfirst(>=(rand()), branch_probs) # TODO what if there is numerical imprecision and sum<1
         j, nothing
     else
