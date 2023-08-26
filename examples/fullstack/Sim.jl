b51304281e1914a3582f1750a025b15d4e8d67ec
Adrian Ariton <adrianariton@192-168-0-154.rdsnet.ro>
2023-08-26 11:42:29 +0300

    set retina scale for windows

--- examples/fullstack/Sim.jl
+++ examples/fullstack/Sim.jl
@@ -10,7 +10,7 @@ include("setup.jl")
 import JSServe.TailwindDashboard as D
 
 # Change between color schemes by uncommentinh lines 17-18
-retina_scale = 2
+retina_scale = 1
 config = Dict(
     :resolution => (retina_scale*1400, retina_scale*700), #used for the main figures
     :smallresolution => (280, 160), #used for the menufigures
