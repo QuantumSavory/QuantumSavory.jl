219d129a049be1f9d5990afd941c6c930308b56c
Abhishek Bhatt <abhishekbhatt730@gmail.com>
2023-09-26 16:14:24 -0400

    corrections

--- src/StatesZoo/zalm_pair/zalm_pair.jl
+++ src/StatesZoo/zalm_pair/zalm_pair.jl
@@ -561,10 +561,10 @@ function cascaded_source_spin(Ns,gA,gB,eAm,eBm,eAs,eBs,eD,Pd,Pdo1,Pdo2,VisF)
 end
 
 
-```
+"""
 Generate symbolic object for the spin-spin density matrix for a 
 cascaded source swapped with emissive spin memories from the paper [prajit2023entangling](@cite)
-```
+"""
 @withmetadata struct ZALMpair <: AbstractTwoQubitState
     Ns::Float64
     gA::Float64
