dd5da70a4d4548e359c29471f356002a314f5477
Abhishek Bhatt <abhishekbhatt730@gmail.com>
2023-11-01 16:17:05 -0400

    Test with `T1Decay` and `T2Dephasing`

--- test/test_quantumchannel.jl
+++ test/test_quantumchannel.jl
@@ -33,8 +33,7 @@ sref = regB.staterefs[1]
 # the above code puts both the qubits of the state in the same register
 @test sref.registers[1] == sref.registers[2]
 
-
-
+## Test with the second constructor
 bell = (Z1⊗Z1 + Z2⊗Z2)/sqrt(2.0)
 regA = Register(2)
 regB = Register(2)
@@ -60,3 +59,51 @@ run(sim)
 sref = regB.staterefs[1]
 
 @test sref.registers[1] == sref.registers[2]
+
+## Test with T1Decay
+
+bell = (Z1⊗Z1 + Z2⊗Z2)/sqrt(2.0)
+regA = Register(2)
+regB = Register(2)
+initialize!((regA[1], regB[2]), bell)
+
+sim = Simulation()
+
+qc = QuantumChannel(sim, 10.0, T1Decay(0.1))
+
+@resumable function alice_node(env, qc)
+    put!(qc, regA[1])
+end
+
+@resumable function bob_node(env, qc)
+    @yield @process take!(env, qc, regB[1])
+end
+
+@process alice_node(sim, qc)
+@process bob_node(sim, qc)
+
+run(sim)
+
+## Test with T2Dephasing
+
+bell = (Z1⊗Z1 + Z2⊗Z2)/sqrt(2.0)
+regA = Register(2)
+regB = Register(2)
+initialize!((regA[1], regB[2]), bell)
+
+sim = Simulation()
+
+qc = QuantumChannel(sim, 10.0, T2Dephasing(0.1))
+
+@resumable function alice_node(env, qc)
+    put!(qc, regA[1])
+end
+
+@resumable function bob_node(env, qc)
+    @yield @process take!(env, qc, regB[1])
+end
+
+@process alice_node(sim, qc)
+@process bob_node(sim, qc)
+
+run(sim)
