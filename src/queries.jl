8d37d35405b3fcf4836bc40604762e2da5518772
Abhishek Bhatt <46929125+Abhishek-1Bhatt@users.noreply.github.com>
2023-11-29 21:23:34 -0500

    Update queries.jl

--- src/queries.jl
+++ src/queries.jl
@@ -15,7 +15,7 @@ struct Wildcard end
 See also: [`query`](@ref), [`tag!`](@ref), [`tag_types`](@ref), [`Wildcard`](@ref)"""
 const W = Wildcard()
 
-"""A wilcard instance for use with the tag querying functionality.
+"""A wildcard instance for use with the tag querying functionality.
 
 See also: [`query`](@ref), [`tag!`](@ref), [`tag_types`](@ref), [`Wildcard`](@ref)"""
 const ❓ = W
