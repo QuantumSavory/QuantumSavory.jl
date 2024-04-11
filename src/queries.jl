c415e558978e62428bcfe3a0627535e6d23d312c
Abhishek Bhatt <abhishekbhatt730@gmail.com>
2024-04-11 10:58:52 -0400

    modify docstring

--- src/queries.jl
+++ src/queries.jl
@@ -278,6 +278,7 @@ end
 $TYPEDSIGNATURES
 
 A [`query`](@ref) for [`RegRef`](@ref) that also deletes the tag from the tag list for the `RegRef`.
+Allows the user to specify order of accessing tags to be FILO or FIFO.
 """
 function querydelete!(ref::RegRef, args...; filo::Bool=true) # TODO there is a lot of code duplication here
     r = query(ref, args...;filo=filo)
