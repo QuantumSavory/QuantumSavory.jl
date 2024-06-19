1cff3f57bd998bbec136bb4363380b0898f9fec4
Stefan Krastanov <stefan@krastanov.org>
2024-06-19 14:47:37 -0400

    cleanup

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,8 +1,8 @@
 using Aqua
 using QuantumSavory
-using QuantumClifford, QuantumOptics, Graphs
 
 Aqua.test_all(QuantumSavory,
     ambiguities=(;broken=true),
-    piracies=(;broken=true)
+    piracies=(;broken=true),
+    stale_deps=(;ignore=[:NetworkLayout]) # needed by package extension but not a condition of its loading
 )
