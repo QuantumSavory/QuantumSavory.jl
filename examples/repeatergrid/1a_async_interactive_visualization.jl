8d0f1e41d0d4e81de6702ffa95de58e85c1028ac
Abhishek Bhatt <abhishekbhatt730@gmail.com>
2024-07-11 11:36:01 -0400

    Add new visualizations and edit readme

--- examples/repeatergrid/1a_async_interactive_visualization.jl
+++ examples/repeatergrid/1a_async_interactive_visualization.jl
@@ -5,7 +5,7 @@ include("setup.jl")
 
 sim, net, graph, consumer, params... = prepare_simulation()
 
-fig = Figure(;size=(1200, 850))
+fig = Figure(;size=(1200, 1100))
 
 # the network part of the visualization
 layout = SquareGrid(cols=:auto, dx=30.0, dy=-30.0)(graph) # provided by NetworkLayout, meant to simplify plotting of graphs in 2D
@@ -19,13 +19,24 @@ txxs = @lift [Point2f(e[1],e[3]) for e in $entlog]
 Δts = @lift length($ts)>1 ? $ts[2:end] .- $ts[1:end-1] : [0.0]
 entlogaxis = Axis(fig[1,2], xlabel="Time", ylabel="Entanglement", title="Entanglement Successes")
 ylims!(entlogaxis, (-1.04,1.04))
-stem!(entlogaxis, tzzs)
+stem!(entlogaxis, txxs)
 histaxis = Axis(fig[2,2], xlabel="ΔTime", title="Histogram of Time to Successes")
 hist!(histaxis, Δts)
 
+avg_fids = @lift cumsum([e[3] for e in $entlog])./($ts) #avg fidelity per unit time
+fid_info = @lift [Point2f(t,f) for (t,f) in zip($ts, $avg_fids)]
+fid_axis = Axis(fig[3,1], xlabel="Time", ylabel="Avg. Fidelity", title="Time evolution of Average Fidelity")
+lines!(fid_axis, fid_info)
+
+num_epr = @lift cumsum(ones(length($entlog)))./($ts) #avg number of pairs per unit time
+num_epr_info = @lift [Point2f(t,n) for (t,n) in zip($ts, $num_epr)]
+num_epr_axis = Axis(fig[3,2], xlabel="Time", title="Avg. Number of Entangled Pairs between Alice and Bob")
+lines!(num_epr_axis, num_epr_info)
+
+
 #  sliders
 sg = SliderGrid( # TODO significant code duplication with the other examples
-    fig[3,1],
+    fig[4,1],
     (label="Probability of success of Entanglement generation at each attempt",
         range=0.001:0.05:1.0, format="{:.3f}", startvalue=0.001),
     (label="Local busy time for swapper",
@@ -57,8 +68,12 @@ step_ts = range(0, 1000, step=0.1)
 record(fig, "grid_sim6x6hv.mp4", step_ts; framerate=10, visible=true) do t
     run(sim, t)
     notify.((obs,entlog))
-    notify.(params)
+    notify.(params) 
     ylims!(entlogaxis, (-1.04,1.04))
     xlims!(entlogaxis, max(0,t-50), 1+t)
+    ylims!(fid_axis, (0, 1.04))
+    xlims!(fid_axis, max(0, t-50), 1+t)
     autolimits!(histaxis)
+    ylims!(num_epr_axis, (0, 4))
+    xlims!(num_epr_axis, max(0, t-50), 1+t)
 end
