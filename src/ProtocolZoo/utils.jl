1986f6b63625c2f6e9b4a09f534ea5365812bb8a
Abhishek Bhatt <abhishekbhatt730@gmail.com>
2024-08-31 15:12:25 -0400

    move 'isolderthan' and 'findfreeslot' back to queries.jl'

--- src/ProtocolZoo/utils.jl
+++ src/ProtocolZoo/utils.jl
@@ -22,51 +22,6 @@ function findswapablequbits(net, node, pred_low, pred_high, choose_low, choose_h
     return (low_nodes[il], high_nodes[ih])
 end
 
-"""Find an empty unlocked slot in a given [`Register`](@ref).
-
-```jldoctest
-julia> reg = Register(3); initialize!(reg[1], X); lock(reg[2]);
-
-julia> findfreeslot(reg) == reg[3]
-true
-
-julia> lock(findfreeslot(reg));
-
-julia> findfreeslot(reg) |> isnothing
-true
-```
-"""
-function findfreeslot(reg::Register; randomize=false, margin=0)
-    n_slots = length(reg.staterefs)
-    freeslots = sum((!isassigned(reg[i]) for i in 1:n_slots))
-    if freeslots >= margin
-        perm = randomize ? randperm : (x->1:x)
-        for i in perm(n_slots)
-            slot = reg[i]
-            islocked(slot) || isassigned(slot) || return slot
-        end
-    end
-end
-
-
-struct NotAssignedError <: Exception # TODO use this in all places where we are throwing something on isassigned (maybe rename to IsAssignedError and check whether we need to keep `f` as part of it (might already be provided by the stacktrace) and check it does not allocate even when the error is not triggered)
-    msg
-    f
-end
-
-function Base.showerror(io::IO, err::NotAssignedError)
-    print(io, "NotAssignedError: ")
-    println(io, err.msg)
-    println("In function: $(err.f)")
-end
-
-"""Check whether a qubit has existed for a time more than its cutoff/coherence time"""
-function isolderthan(slot::RegRef, time_left::Float64)
-    if !isassigned(slot) throw(NotAssignedError("Slot must be assigned with a quantum state before checking coherence.", isolderthan)) end
-    id = query(slot, QuantumSavory.ProtocolZoo.EntanglementCounterpart, ❓, ❓).id
-    slot_time  = slot.reg.tag_info[id][3]
-    return (now(get_time_tracker(slot))) - slot_time > time_left
-end
 
 """
 $TYPEDEF
