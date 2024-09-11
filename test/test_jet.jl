fb352b0ad28ef09ad10ff3b603f3d36290ceb596
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-09-11 18:38:57 +0200

    Ignore JET reports from GraphsMatching

--- test/test_jet.jl
+++ test/test_jet.jl
@@ -1,6 +1,6 @@
 using Test
 using QuantumSavory, JET
-using DiffEqBase, Graphs, JumpProcesses, Makie, ResumableFunctions, ConcurrentSim, QuantumOptics, QuantumOpticsBase, QuantumClifford, Symbolics, WignerSymbols
+using DiffEqBase, Graphs, JumpProcesses, Makie, ResumableFunctions, ConcurrentSim, QuantumOptics, QuantumOpticsBase, QuantumClifford, Symbolics, WignerSymbols, GraphsMatching
 
 rep = report_package("QuantumSavory";
     ignored_modules=(
@@ -16,10 +16,11 @@ rep = report_package("QuantumSavory";
         AnyFrameModule(ResumableFunctions),
         AnyFrameModule(ConcurrentSim),
         AnyFrameModule(WignerSymbols),
+        AnyFrameModule(GraphsMatching),
     ))
 
 @show length(JET.get_reports(rep))
 @show rep
 
-@test length(JET.get_reports(rep)) <= 85
+@test length(JET.get_reports(rep)) <= 79
 @test_broken length(JET.get_reports(rep)) == 0
