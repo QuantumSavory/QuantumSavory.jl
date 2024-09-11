70045ba08fb98282cae980dec070a479967f1c6b
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-09-11 20:52:42 +0200

    Fix a typo

--- test/test_jet.jl
+++ test/test_jet.jl
@@ -24,5 +24,5 @@ rep = report_package("QuantumSavory";
 @show length(JET.get_reports(rep))
 @show rep
 
-@test length(JET.get_reports(rep)) <= 37
+@test length(JET.get_reports(rep)) <= 36
 @test_broken length(JET.get_reports(rep)) == 0
