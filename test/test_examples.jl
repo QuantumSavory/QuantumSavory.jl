30afb15dba557bb1934f11d8321a2589d76b52d0
Bryan Kaperick <bryankaperick@gmail.com>
2024-09-25 13:38:13 +0200

    Need setup files for example tests

--- test/test_examples.jl
+++ test/test_examples.jl
@@ -7,12 +7,14 @@ end
 
 @testitem "Examples - congestionchain" tags=[:examples] begin
     if get(ENV,"QUANTUMSAVORY_PLOT_TEST","")=="true"
+        include("setup_plotting.jl")
         include("../examples/congestionchain/1_visualization.jl")
     end
 end
 
 @testitem "Examples - firstgenrepeater" tags=[:examples] begin
     if get(ENV,"QUANTUMSAVORY_PLOT_TEST","")=="true"
+        include("setup_plotting.jl")
         include("../examples/firstgenrepeater/1_entangler_example.jl")
         include("../examples/firstgenrepeater/2_swapper_example.jl")
         include("../examples/firstgenrepeater/3_purifier_example.jl")
@@ -25,6 +27,7 @@ end
 
 @testitem "Examples - firstgenrepeater_v2" tags=[:examples] begin
     if get(ENV,"QUANTUMSAVORY_PLOT_TEST","")=="true"
+        include("setup_plotting.jl")
         include("../examples/firstgenrepeater_v2/1_entangler_example.jl")
         include("../examples/firstgenrepeater_v2/2_swapper_example.jl")
     end
@@ -33,12 +36,14 @@ end
 @testitem "Examples - simpleswitch" tags=[:examples] begin
     if get(ENV,"QUANTUMSAVORY_PLOT_TEST","")=="true"
         # TODO-MATCHING due to the dependence on BlossomV.jl this has trouble installing. See https://github.com/JuliaGraphs/GraphsMatching.jl/issues/14
+        #include("setup_plotting.jl")
         #include("../examples/simpleswitch/1_interactive_visualization.jl")
     end
 end
 
 @safetestset "repeatergrid" begin
     if get(ENV, "QUANTUMSAVORY_PLOT_TEST","")=="true"
+        include("setup_plotting.jl")
         include("../examples/repeatergrid/1a_async_interactive_visualization.jl")
         include("../examples/repeatergrid/2a_sync_interactive_visualization.jl")
     end
