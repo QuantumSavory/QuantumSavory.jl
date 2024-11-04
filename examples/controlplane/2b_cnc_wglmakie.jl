41b0271c63c6914b5ec9d3f7c3aa0ed23cd20223
Abhishek Bhatt <abhishekbhatt730@gmail.com>
2024-11-04 11:34:37 -0500

    move path selection to controller and related modifications

--- examples/controlplane/2b_cnc_wglmakie.jl
+++ examples/controlplane/2b_cnc_wglmakie.jl
@@ -7,11 +7,10 @@ include("setup.jl")
 
 const custom_css = Bonito.DOM.style("ul {list-style: circle !important;}") # TODO remove after fix of bug in JSServe https://github.com/SimonDanisch/JSServe.jl/issues/178
 
-phys_graph = PhysicalGraph(graph, 1, 8, regsize)
-controller = Controller(sim, net, 6, phys_graph)
+controller = Controller(sim, net, 6, zeros(8,8))
 @process controller()
 
-req_gen = RequestGenerator(sim, net, 1, 8, 6, phys_graph)
+req_gen = RequestGenerator(sim, net, 1, 8, 6)
 @process req_gen()
 
 consumer = EntanglementConsumer(sim, net, 1, 8)
