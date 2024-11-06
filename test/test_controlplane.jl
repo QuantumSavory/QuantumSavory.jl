f344058ece4eec2dc957783f0552d7c7b410f95d
Abhishek Bhatt <abhishekbhatt730@gmail.com>
2024-11-06 11:22:38 -0500

    add new protocol to tests

--- test/test_controlplane.jl
+++ test/test_controlplane.jl
@@ -1,4 +1,4 @@
-@testitem "Control Protocol" tags=[:controlplane] begin
+@testitem "Control Protocols" tags=[:controlplane] begin
 
 using QuantumSavory
 using QuantumSavory.ProtocolZoo
@@ -30,6 +30,40 @@ sim = get_time_tracker(net)
 
 # controller
 controller = Controller(sim, net, 6, zeros(8,8))
+@process controller()
+
+# RequestGenerator for the user pair (1,8)
+req_gen = RequestGenerator(sim, net, 1, 8, 6)
+@process req_gen()
+
+# consumer
+consumer = EntanglementConsumer(sim, net, 1, 8)
+@process consumer()
+
+# entanglement and request trackers, cutoff protocol
+for v in 1:8
+    etracker = EntanglementTracker(sim, net, v)
+    rtracker = RequestTracker(sim, net, v)
+    cutoff = CutoffProt(sim, net, v)
+    @process etracker()
+    @process rtracker()
+    @process cutoff()
+end
+
+run(sim, 1000)
+
+for i in 1:length(consumer.log)
+    @test consumer.log[i][2] ≈ 1.0
+    @test consumer.log[i][3] ≈ 1.0
+end
+
+#### Connection less controller
+
+net = RegisterNet(graph, [Register(regsize) for i in 1:8])
+sim = get_time_tracker(net)
+
+# controller
+controller = CLController(sim, net, 6)
 @process controller()
 
 # RequestGenerator for the user pair (1,8)
