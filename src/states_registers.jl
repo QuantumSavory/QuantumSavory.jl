51887e5cd8f4a973e6f97cd498ebfae961966573
Stefan Krastanov <stefan@krastanov.org>
2024-12-19 12:24:39 -0500

    remove outdated comments and fix whitespace

--- src/states_registers.jl
+++ src/states_registers.jl
@@ -1,7 +1,5 @@
-#using QuantumSavory: AsymmetricSemaphore
 # TODO better constructors
 # TODO am I overusing Ref
-
 struct StateRef
     state::Base.RefValue{Any} # TODO it would be nice if this was not abstract but `uptotime!` converts between types... maybe make StateRef{T} state::RefValue{T} and a new function that swaps away the backpointers in the appropriate registers
     registers::Vector{Any} # TODO Should be Vector{Register}, but right now we occasionally set it to nothing to deal with padded storage
