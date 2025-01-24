97c9e4dac6a4edf906e218720e067550a26a0f06
Stefan Krastanov <stefan@krastanov.org>
2025-01-24 11:20:37 -0500

    Avoid method-override warnings in tests by using separate modules (#177)
    Looks good to me!

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,7 @@ function testfilter(tags)
     if get(ENV,"QUANTUMSAVORY_PLOT_TEST","")!="true"
         push!(exclude, :plotting_cairo)
         push!(exclude, :plotting_gl)
+        push!(exclude, :examples_plotting)
         push!(exclude, :doctests)
     end
     if get(ENV,"JET_TEST","")!="true"
@@ -15,5 +16,14 @@ function testfilter(tags)
     return all(!in(exclude), tags)
 end
 
+if get(ENV,"QUANTUMSAVORY_PLOT_TEST","")=="true"
+    include("setup_plotting.jl") # avoid the installation cost for GLMakie unless necessary
+end
+
 println("Starting tests with $(Threads.nthreads()) threads out of `Sys.CPU_THREADS = $(Sys.CPU_THREADS)`...")
 @run_package_tests filter=ti->testfilter(ti.tags) verbose=true
+
+if get(ENV,"QUANTUMSAVORY_PLOT_TEST","")=="true"
+    import GLMakie
+    GLMakie.closeall() # to avoid errors when running headless
+end
