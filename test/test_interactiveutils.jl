88d419c690f25efbad6ef03bfa9aa46145d5e417
Stefan Krastanov <github.acc@krastanov.org>
2025-08-22 15:51:03 -0400

    typo

--- test/test_interactiveutils.jl
+++ test/test_interactiveutils.jl
@@ -3,7 +3,7 @@ using QuantumSavory
 using QuantumSavory.ProtocolZoo
 using InteractiveUtils
 
-bgs = QuantumSavory.available_backgrounds()
+bgs = QuantumSavory.available_background_types()
 QuantumSavory.constructor_metadata.(bgs)
 prots = QuantumSavory.ProtocolZoo.available_protocol_types()
 QuantumSavory.constructor_metadata.(prots)
