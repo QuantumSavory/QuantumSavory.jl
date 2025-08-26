c65547b3c22e54585dfd1ca5c8c1a5946ceb5d49
Stefan Krastanov <stefan@krastanov.org>
2025-08-26 09:57:02 -0400

    fixes to interactiveutils ext (#258)

--- test/test_interactiveutils.jl
+++ test/test_interactiveutils.jl
@@ -7,5 +7,9 @@ bgs = QuantumSavory.available_background_types()
 QuantumSavory.constructor_metadata.([bg.type for bg in bgs])
 prots = QuantumSavory.ProtocolZoo.available_protocol_types()
 QuantumSavory.constructor_metadata.([p.type for p in prots])
+slots = QuantumSavory.available_slot_types()
+@test length(bgs)>1
+@test length(prots)>1
+@test length(slots)>1
 
 end
