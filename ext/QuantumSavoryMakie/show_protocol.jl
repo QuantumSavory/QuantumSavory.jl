ee6340a0bfcc04d5727a3df9449239dda016d23e
Stefan Krastanov <stefan@krastanov.org>
2025-09-01 02:29:47 -0400

    show image/png for state and protocol

---
+++ ext/QuantumSavoryMakie/show_protocol.jl
@@ -0,0 +1,14 @@
+function Base.show(io::IO, m::MIME"image/png", prot::QuantumSavory.ProtocolZoo.AbstractProtocol)
+    f = Figure()
+    protshowimage(f, prot)
+    show(io, m, f)
+end
+
+"""Similar to `show(io, ::MIME"", ...)`, but private to avoid piracy. Instead of an IO instance, it takes a Makie axis."""
+function protshowimage(subfig, prot)
+    a = Axis(subfig[1,1])
+    hidedecorations!(a)
+    hidespines!(a)
+    text = "protocol of type\n$(typeof(prot))\ndoes not support rich visualization"
+    text!(a,0,0;text,align=(:center,:center))
+end
---
+++ ext/QuantumSavoryMakie/show_state.jl
@@ -0,0 +1,29 @@
+function Base.show(io::IO, m::MIME"image/png", s::StateRef)
+    f = Figure()
+    stateshowimage(f,QuantumSavory.quantumstate(s),s)
+    show(io, m, f)
+end
+
+"""Similar to `show(io, ::MIME"", ...)`, but private to avoid piracy. Instead of an IO instance, it takes a Makie axis."""
+function stateshowimage(subfig, state, stateref)
+    a = Axis(subfig[1,1])
+    hidedecorations!(a)
+    hidespines!(a)
+    text = "state of type\n$(typeof(state))\ndoes not support rich visualization"
+    text!(a,0,0;text,align=(:center,:center))
+end
+
+function stateshowimage(subfig, state::QuantumClifford.MixedDestabilizer, stateref)
+    stab = QuantumClifford.stabilizerview(state)
+    names = [
+        (isnothing(QuantumSavory.name(s.reg)) ? QuantumSavory.name(s.reg) : string(s.reg.netindex[]))*"[$(s.idx)]"
+        for s in QuantumSavory.slots(stateref)
+        ]
+    subfig,ax,p = QuantumClifford.stabilizerplot_axis(subfig, stab)
+    #ax.xticksvisible = true
+    ax.xticklabelsvisible = true
+    ax.xticks = (1:length(names), names)
+    ax.xticklabelrotation = pi/2*0.8
+    ax.yticks = (Int[], String[])
+    subfig
+end
