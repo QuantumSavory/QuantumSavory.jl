6d7210a6f218471daa54aa84b5966b3470f986ec
Stefan Krastanov <stefan@krastanov.org>
2025-09-03 10:40:39 -0400

    typo

--- src/QuantumSavory.jl
+++ src/QuantumSavory.jl
@@ -103,17 +103,17 @@ function __init__()
             elseif exc.f === registernetplot!
                 println(io, "\n`registernetplot!` is implemented as an optional extension dependent on `Makie`; please make sure `Makie` is installed and imported first.")
             elseif exc.f === registernetplot_axis
-                println(io, "\n`registernetplot_axis` is implemeted as an optional extension dependent on `Makie`; please make sure `Makie` is installed and imported first.")
+                println(io, "\n`registernetplot_axis` is implemented as an optional extension dependent on `Makie`; please make sure `Makie` is installed and imported first.")
             elseif exc.f === resourceplot_axis
-                println(io, "\n`resourceplot_axis` is implemeted as an optional extension dependent on `Makie`; please make sure `Makie` is installed and imported first.")
+                println(io, "\n`resourceplot_axis` is implemented as an optional extension dependent on `Makie`; please make sure `Makie` is installed and imported first.")
             elseif exc.f === generate_map
-                println(io, "\n`generate_map` is implemeted as an optional extension dependent on `Tyler`; please make sure `Tyler` is installed and imported first.")
+                println(io, "\n`generate_map` is implemented as an optional extension dependent on `Tyler`; please make sure `Tyler` is installed and imported first.")
             elseif exc.f === available_slot_types
-                println(io, "\n`available_slot_types` is implemeted as an optional extension dependent on `InteractiveUtils` and `REPL`; please make sure `InteractiveUtils` and `REPL` are installed and imported first.")
+                println(io, "\n`available_slot_types` is implemented as an optional extension dependent on `InteractiveUtils` and `REPL`; please make sure `InteractiveUtils` and `REPL` are installed and imported first.")
             elseif exc.f === available_background_types
-                println(io, "\n`available_background_types` is implemeted as an optional extension dependent on `InteractiveUtils` and `REPL`; please make sure `InteractiveUtils` and `REPL` are installed and imported first.")
+                println(io, "\n`available_background_types` is implemented as an optional extension dependent on `InteractiveUtils` and `REPL`; please make sure `InteractiveUtils` and `REPL` are installed and imported first.")
             elseif exc.f === constructor_metadata
-                println(io, "\n`constructor_metadata` is implemeted as an optional extension dependent on `InteractiveUtils` and `REPL`; please make sure `InteractiveUtils` and `REPL` are installed and imported first.")
+                println(io, "\n`constructor_metadata` is implemented as an optional extension dependent on `InteractiveUtils` and `REPL`; please make sure `InteractiveUtils` and `REPL` are installed and imported first.")
             end
         end
     end
