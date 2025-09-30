da8513fb20483e76d6128c808c6defbc9ce6914e
Stefan Krastanov <stefan@krastanov.org>
2025-09-30 00:31:36 -0400

    polish the genqo state_explorer

--- src/StatesZoo/state_explorer.jl
+++ src/StatesZoo/state_explorer.jl
@@ -2,10 +2,23 @@
 
 """An interactive explorer for two-qubit states. It returns a new figure.
 
-Requires a Makie plotting backend to be imported."""
+Requires a Makie plotting backend to be imported.
+
+```julia
+using GLMakie
+using QuantumSavory
+using QuantumSavory.StatesZoo
+stateexplorer(TheStateTypeYouWant) # an interactive Makie figure will be returned
+```
+
+See also [`stateexplorer!`](@ref).
+"""
 function stateexplorer end
 
 """An interactive explorer for two-qubit states. It modifies the given figure.
 
-Requires a Makie plotting backend to be imported."""
+Requires a Makie plotting backend to be imported.
+
+See also [`stateexplorer`](@ref).
+"""
 function stateexplorer! end
