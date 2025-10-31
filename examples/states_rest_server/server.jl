17cb4ee0f2cf0a56a818ae593f4139ea8fcc7773
Stefan Krastanov <stefan@krastanov.org>
2025-10-30 20:42:54 -0400

    external docs url

--- examples/states_rest_server/server.jl
+++ examples/states_rest_server/server.jl
@@ -242,5 +242,7 @@ if abspath(PROGRAM_FILE) == @__FILE__
     port = parse(Int, Base.get(ENV, "QS_STATES_REST_SERVER_PORT", "8080"))
     host = Base.get(ENV, "QS_STATES_REST_SERVER_IP", "127.0.0.1")
     external_url = Base.get(ENV, "QS_STATES_REST_SERVER_PROXY", nothing)
-    serve(;port, host, external_url)
+    external_url = Base.get(ENV, "QS_STATES_REST_SERVER_PROXY", nothing)
+    docs_path = Base.get(ENV, "QS_STATES_REST_SERVER_DOCPATH", "/docs")
+    serve(;port, host, external_url, docs_path)
 end
