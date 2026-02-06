714420abd937f74f37f5121016850a5482cd992f
Stefan Krastanov <stefan@krastanov.org>
2026-02-06 18:53:10 -0500

    misc cherrypicks from #266

--- src/ProtocolZoo/cutoff.jl
+++ src/ProtocolZoo/cutoff.jl
@@ -36,45 +36,53 @@ CutoffProt(net::RegisterNet, node::Int; kwargs...) = CutoffProt(get_time_tracker
 
 @resumable function (prot::CutoffProt)()
     reg = prot.net[prot.node]
+    for slot in reg
+        @process per_slot_cutoff(prot.sim, slot, prot)
+    end
+end
+
+@resumable function per_slot_cutoff(sim, slot::RegRef, prot::CutoffProt)
+    empty_query = false
     while true
-        for slot in reg # TODO these should be done in parallel, otherwise we will be waiting on each slot, greatly slowing down the cutoffs
-            islocked(slot) && continue
-            @yield lock(slot)
-            info = query(slot, EntanglementCounterpart, ❓, ❓)
-            if isnothing(info)
-                unlock(slot)
-                continue
+        if empty_query
+            if isnothing(prot.period)
+                @yield onchange(slot, Tag) # TODO this should be just for the slot, not for the whole register
+            else
+                @yield timeout(prot.sim, prot.period::Float64)
             end
-            if now(prot.sim) - reg.tag_info[info.id][3] > prot.retention_time # TODO this should be part of the query interface, not using non-public implementation details
-                untag!(slot, info.id)
-                traceout!(slot)
-                msg = Tag(EntanglementDelete, prot.node, slot.idx, info.tag[2], info.tag[3])
-                tag!(slot, msg)
-                (prot.announce) && put!(channel(prot.net, prot.node=>msg[4]; permit_forward=true), msg)
-                @debug "CutoffProt @$(prot.node): Send message to $(msg[4]) | message=`$msg` | time=$(now(prot.sim))"
-            end
-
-            # TODO the tag deletions below are not necessary when announce=true and EntanglementTracker is running on other nodes. Verify the veracity of that statement, make tests for both cases, and document.
-
-            # delete old history tags
-            info = query(slot, EntanglementHistory, ❓, ❓, ❓, ❓, ❓;filo=false) # TODO we should have a warning if `queryall` returns more than one result -- what does it even mean to have multiple history tags here
-            if !isnothing(info) && now(prot.sim) - reg.tag_info[info.id][3] > prot.retention_time # TODO this should be part of the query interface, not using non-public implementation details
-                untag!(slot, info.id)
-            end
-
-            # delete old EntanglementDelete tags
-            # TODO Why do we have separate entanglementhistory and entanglementupdate but we have only a single entanglementdelete that serves both roles? We should probably have both be pairs of tags, for consistency and ease of reasoning
-            info = query(slot, EntanglementDelete, prot.node, slot.idx , ❓, ❓) # TODO we should have a warning if `queryall` returns more than one result -- what does it even mean to have multiple delete tags here
-            if !isnothing(info) && now(prot.sim) - reg.tag_info[info.id][3] > prot.retention_time # TODO this should be part of the query interface, not using non-public implementation details
-                untag!(slot, info.id)
-            end
-
+        end
+        @yield lock(slot)
+        info = query(slot, EntanglementCounterpart, ❓, ❓)
+        if isnothing(info) || now(sim) - info.time < prot.retention_time
+            empty_query = true
             unlock(slot)
+            continue
         end
-        if isnothing(prot.period)
-            @yield onchange(reg, Tag)
-        else
-            @yield timeout(prot.sim, prot.period::Float64)
+        println("$(now(sim)) $slot info $info")
+
+        untag!(slot, info.id)
+        traceout!(slot)
+        println("$(now(sim)) $slot traced out")
+        msg = Tag(EntanglementDelete, prot.node, slot.idx, info.tag[2], info.tag[3])
+        tag!(slot, msg)
+        (prot.announce) && put!(channel(prot.net, prot.node=>msg[4]; permit_forward=true), msg)
+        @debug "CutoffProt @$(prot.node): Send message to $(msg[4]) | message=`$msg` | time=$(now(prot.sim))"
+
+        # TODO the tag deletions below are not necessary when announce=true and EntanglementTracker is running on other nodes. Verify the veracity of that statement, make tests for both cases, and document.
+
+        # delete old history tags
+        info = query(slot, EntanglementHistory, ❓, ❓, ❓, ❓, ❓;filo=false) # TODO we should have a warning if `queryall` returns more than one result -- what does it even mean to have multiple history tags here
+        if !isnothing(info) && now(prot.sim) - info.time > prot.retention_time
+            untag!(slot, info.id)
         end
+
+        # delete old EntanglementDelete tags
+        # TODO Why do we have separate entanglementhistory and entanglementupdate but we have only a single entanglementdelete that serves both roles? We should probably have both be pairs of tags, for consistency and ease of reasoning
+        info = query(slot, EntanglementDelete, prot.node, slot.idx , ❓, ❓) # TODO we should have a warning if `queryall` returns more than one result -- what does it even mean to have multiple delete tags here
+        if !isnothing(info) && now(prot.sim) - info.time > prot.retention_time
+            untag!(slot, info.id)
+        end
+
+        unlock(slot)
     end
 end
