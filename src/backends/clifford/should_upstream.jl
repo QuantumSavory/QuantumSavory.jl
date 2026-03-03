2351b5aecf7ecf1e5a7702f8d4fc8a5a8a5d1e92
Stefan Krastanov <stefan@krastanov.org>
2026-03-03 19:43:19 +0000

    Drop local Clifford should_upstream patch now moved to QuantumSymbolics

--- src/backends/clifford/should_upstream.jl
+++ src/backends/clifford/should_upstream.jl
@@ -2,15 +2,15 @@
 struct QCGateSequence <: QuantumClifford.AbstractSymbolicOperator
     gates # TODO constructor that flattens nested QCGateSequence
 end
-function QuantumClifford.apply!(state::QuantumClifford.MixedDestabilizer, indices::AbstractVector{Int}, gseq::QCGateSequence)
+function QuantumClifford.apply!(state::QuantumClifford.MixedDestabilizer, gseq::QCGateSequence, indices::AbstractVector{Int})
     for g in gseq.gates[end:-1:begin]
-        apply_popindex!(state, indices, g)
+        apply_popindex!(state, g, indices)
     end
     state
 end
-apply_popindex!(state, indices::AbstractVector{Int}, g::Type{<:QuantumClifford.AbstractSingleQubitOperator}) =
+apply_popindex!(state, g::QuantumClifford.AbstractSingleQubitOperator, indices::AbstractVector{Int}) =
     QuantumClifford.apply!(state, g(pop!(indices)::Int))
-apply_popindex!(state, indices::AbstractVector{Int}, g::Type{<:QuantumClifford.AbstractTwoQubitOperator}) =
+apply_popindex!(state, g::QuantumClifford.AbstractTwoQubitOperator, indices::AbstractVector{Int}) =
     QuantumClifford.apply!(state, g(pop!(indices)::Int, pop!(indices)::Int))
 
 projector(state::QuantumClifford.Stabilizer) = projector(StabilizerState(state)) # convert to a type that QuantumSymbolics can handle
