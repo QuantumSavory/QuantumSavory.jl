fdfba625cb6baa2c036b1de18e7589b1133f3f4b
Stefan Krastanov <stefan@krastanov.org>
2026-03-21 16:10:59 +0000

    Drop stale delete messages in EntanglementTracker

--- src/ProtocolZoo/ProtocolZoo.jl
+++ src/ProtocolZoo/ProtocolZoo.jl
@@ -413,6 +413,11 @@ EntanglementTracker(net::RegisterNet, node::Int) = EntanglementTracker(get_time_
                     continue
                 end
 
+                if isnothing(updategate)
+                    @debug "EntanglementTracker @$(prot.node): stale delete message=`$msg` is dropped"
+                    continue
+                end
+
                 error("`EntanglementTracker` on node $(prot.node) received a message $(msg) that it does not know how to handle (due to the absence of corresponding `EntanglementCounterpart` or `EntanglementHistory` or `EntanglementDelete` tags). This might happen due to a race condition from infinitely fast classical messages. Make sure that `classical_delay` in `RegisterNet` is not zero. This might also happen due to `CutoffProt` deleting qubits while swaps are happening. Make sure that the retention times in `CutoffProt` are sufficiently larger than the `agelimit` in `SwapperProt`. Otherwise, this is a bug in the protocol and should not happen -- please report an issue at QuantumSavory's repository.")
             end
         end
--- test/examples/repeatergrid_1a_tests.jl
+++ test/examples/repeatergrid_1a_tests.jl
@@ -1,7 +1,5 @@
 using Test
 
 @testset "Examples - repeatergrid 1a" begin
-    @test_broken false
-    # this test consistently fails due to
-    #include("../../examples/repeatergrid/1a_async_interactive_visualization.jl")
+    include("../../examples/repeatergrid/1a_async_interactive_visualization.jl")
 end
