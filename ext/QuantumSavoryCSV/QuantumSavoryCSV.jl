a1c140e47c1ab054b05a00a4ed2b8f61d908ee68
JoseRodriguezRomero <jose.antonio.rodrigues.romero@gmail.com>
2026-04-30 16:19:53 -0400

    Small changes
     - ``QuantumSavoryCSV ``no longer requires ``DataFrames`` as a dependency.
      - Both ``QuantumSavoryCSV`` and ``QuantumSavoryHDF5`` use the property names of ``_log`` in ``EntanglementConsumer`` instead of hard coded names.

--- ext/QuantumSavoryCSV/QuantumSavoryCSV.jl
+++ ext/QuantumSavoryCSV/QuantumSavoryCSV.jl
@@ -2,11 +2,11 @@ module QuantumSavoryCSV
 
 import QuantumSavory
 using QuantumSavory.ProtocolZoo
-import FileIO, CSV, DataFrames
+import FileIO, CSV
 
 function FileIO.save(save_file::FileIO.File{FileIO.DataFormat{:CSV},String}, prot::EntanglementConsumer; metadata::Union{Dict{String,Any},Nothing} = nothing)
     if !isempty(prot._log)
-        CSV.write(save_file, DataFrames.DataFrame(prot._log, ["t", "obs1", "obs2"]))
+        CSV.write(save_file, prot._log)
     end
 end
 
--- ext/QuantumSavoryHDF5/QuantumSavoryHDF5.jl
+++ ext/QuantumSavoryHDF5/QuantumSavoryHDF5.jl
@@ -6,19 +6,16 @@ import FileIO, HDF5
 
 function FileIO.save(save_file::FileIO.File{FileIO.DataFormat{:HDF5},String}, prot::EntanglementConsumer; metadata::Union{Dict{String,Any},Nothing} = nothing)
     HDF5.h5open(save_file.filename, "w") do file
-        write(file, "t", [log.t for log in prot._log])
-        write(file, "obs1", [log.obs1 for log in prot._log])
-        write(file, "obs2", [log.obs2 for log in prot._log])
-
         if !isnothing(metadata)
-            metadata_keys = collect(keys(metadata))
-            metadata_values = Vector{String}()
-            for key in metadata_keys
-                push!(metadata_values, string(metadata[key]))
+            for metadata_key in keys(metadata)
+                write(file, string(metadata_key), string(metadata[metadata_key]))
             end
+        end
 
-            write(file, "metadata_keys", metadata_keys)
-            write(file, "metadata_values", metadata_values)
+        if !isempty(prot._log)
+            for property_name in propertynames(prot._log[1])
+                write(file, string(property_name), [getfield(log, property_name) for log in prot._log])
+            end
         end
     end
 end
