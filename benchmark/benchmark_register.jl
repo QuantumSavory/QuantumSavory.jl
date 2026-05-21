252e5f3a7cb84dd8fd0bbaf5b1b2fbf1bd7faba3
梁嘉呈 <liangjiacheng@liangjiachengdeMac-mini.local>
2026-05-21 23:44:54 +0800

    benchmark: add focused register microbenchmarks for issue #131

--- benchmark/benchmark_register.jl
+++ benchmark/benchmark_register.jl
@@ -74,3 +74,76 @@ function register_creation_and_initialization()
     @assert nsubsystems(net[i].staterefs[2]) == 2
 end
 SUITE["register"]["creation_and_initialization"]["from_tests"] = @benchmarkable register_creation_and_initialization()
+
+SUITE["register"]["micro"] = BenchmarkGroup(["micro"])
+
+const _traits = [Qubit(), Qubit(), Qubit()]
+const _qc_repr = [QuantumOpticsRepr(), CliffordRepr(), CliffordRepr()]
+const _qmc_repr = [QuantumOpticsRepr(), QuantumMCRepr(), QuantumMCRepr()]
+const _backgrounds = [T2Dephasing(1.0), T2Dephasing(1.0), T2Dephasing(1.0)]
+
+create_register_net_plain() = RegisterNet([Register(_traits), Register(_traits, _qc_repr), Register(_traits, _qmc_repr)])
+create_register_net_with_backgrounds() = RegisterNet([Register(_traits, _backgrounds), Register(_traits, _qc_repr, _backgrounds), Register(_traits, _qmc_repr, _backgrounds)])
+
+function initialize_pair_qo!()
+    net = create_register_net_plain()
+    initialize!(net[1,2])
+    initialize!(net[1,3], X1)
+    apply!([net[1,2], net[1,3]], CNOT)
+    @assert net[1].staterefs[2].state[] isa Ket
+    return nothing
+end
+
+function initialize_pair_qc!()
+    net = create_register_net_plain()
+    initialize!(net[2,2])
+    initialize!(net[2,3], X1)
+    apply!([net[2,2], net[2,3]], CNOT)
+    @assert net[2].staterefs[2].state[] isa MixedDestabilizer
+    return nothing
+end
+
+function initialize_pair_qmc!()
+    net = create_register_net_plain()
+    initialize!(net[3,2])
+    initialize!(net[3,3], X1)
+    apply!([net[3,2], net[3,3]], CNOT)
+    @assert net[3].staterefs[2].state[] isa Ket
+    return nothing
+end
+
+function initialize_pair_qo_background!()
+    net = create_register_net_with_backgrounds()
+    initialize!(net[1,2], time=1.0)
+    initialize!(net[1,3], X1, time=2.0)
+    apply!([net[1,2], net[1,3]], CNOT, time=3.0)
+    @assert net[1].staterefs[2].state[] isa Operator
+    return nothing
+end
+
+function initialize_pair_qc_background!()
+    net = create_register_net_with_backgrounds()
+    initialize!(net[2,2], time=1.0)
+    initialize!(net[2,3], X1, time=2.0)
+    apply!([net[2,2], net[2,3]], CNOT, time=3.0)
+    @assert net[2].staterefs[2].state[] isa MixedDestabilizer
+    return nothing
+end
+
+function initialize_pair_qmc_background!()
+    net = create_register_net_with_backgrounds()
+    initialize!(net[3,2], time=1.0)
+    initialize!(net[3,3], X1, time=2.0)
+    apply!([net[3,2], net[3,3]], CNOT, time=3.0)
+    @assert nsubsystems(net[3].staterefs[2]) == 2
+    return nothing
+end
+
+SUITE["register"]["micro"]["create_net_plain"] = @benchmarkable create_register_net_plain()
+SUITE["register"]["micro"]["create_net_backgrounds"] = @benchmarkable create_register_net_with_backgrounds()
+SUITE["register"]["micro"]["init_pair_qo"] = @benchmarkable initialize_pair_qo!()
+SUITE["register"]["micro"]["init_pair_qc"] = @benchmarkable initialize_pair_qc!()
+SUITE["register"]["micro"]["init_pair_qmc"] = @benchmarkable initialize_pair_qmc!()
+SUITE["register"]["micro"]["init_pair_qo_background"] = @benchmarkable initialize_pair_qo_background!()
+SUITE["register"]["micro"]["init_pair_qc_background"] = @benchmarkable initialize_pair_qc_background!()
+SUITE["register"]["micro"]["init_pair_qmc_background"] = @benchmarkable initialize_pair_qmc_background!()
