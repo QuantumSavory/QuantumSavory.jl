f226b2fd9d3a68241b81875187633db8ca872084
Stefan Krastanov <stefan@krastanov.org>
2026-07-17 00:52:22 +0000

    Share state-reference offset remapping

--- src/baseops/observable.jl
+++ src/baseops/observable.jl
@@ -6,15 +6,17 @@ of the `obs` observable (using the appropriate formalism, depending on the state
 representation in the given registers).
 """
 function observable(regs::Base.AbstractVecOrTuple{Register}, indices::Base.AbstractVecOrTuple{Int}, obs; something=nothing, time=nothing)
-    staterefs = [r.staterefs[i] for (r,i) in zip(regs, indices)]
-    any(isnothing, staterefs) && return something
+    staterefs = StateRef[]
+    for (r, i) in zip(regs, indices)
+        ref = r.staterefs[i]
+        isnothing(ref) && return something
+        push!(staterefs, ref)
+    end
     !isnothing(time) && uptotime!(regs, indices, time)
-    unique_staterefs = unique(objectid, staterefs)
+    unique_staterefs, offsets = unique_staterefs_with_offsets(staterefs)
     state = length(unique_staterefs) == 1 ? unique_staterefs[1].state[] :
             subsystemcompose([s.state[] for s in unique_staterefs]...)
-    offsets = [0, cumsum(nsubsystems_padded.(unique_staterefs))...]
-    state_indices = [r.stateindices[i] +
-                     offsets[findfirst(s -> s === r.staterefs[i], unique_staterefs)]
+    state_indices = [r.stateindices[i] + offsets[r.staterefs[i]]
                      for (r,i) in zip(regs, indices)]
     observable(state, state_indices, obs)
 end
